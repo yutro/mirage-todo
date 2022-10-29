dcb5c0ba7e4671fb4e5312eced75ce910e612379
Yury A Troynov <icokie@yandex.ru>
2022-10-29 17:27:25 +0300

    tmp

--- src/pages/Lists/Tasks/Tasks.tsx
+++ src/pages/Lists/Tasks/Tasks.tsx
@@ -1,6 +1,12 @@
+import { useQueryClient } from "@tanstack/react-query";
 import classNames from "classnames";
 import { Link, useParams } from "react-router-dom";
-import { useTodoListByIdQuery } from "../../../generated";
+import {
+	namedOperations,
+	useDeleteToDoListMutation,
+	useDeleteToDoMutation,
+	useTodoListByIdQuery,
+} from "../../../generated";
 import {
 	H1,
 	Header,
@@ -16,14 +22,26 @@ export const Tasks = (): JSX.Element => {
 
 	if (!listId) throw new Error("Tasks: required listId is missing");
 
-	const { data, error, isFetching } = useTodoListByIdQuery({ listId });
+	const queryClient = useQueryClient();
+
+	const { data, error, isFetching, refetch } = useTodoListByIdQuery({ listId });
+	const { mutate: deleteToDoListById } = useDeleteToDoListMutation();
+	const { mutate: deleteToDoById } = useDeleteToDoMutation({
+		onSuccess: () => refetch(),
+	});
 
 	if (error) return <div>error</div>;
 
 	return (
 		<Layout className="flex flex-col">
-			<Header>
+			<Header className="">
 				<H1>{data?.todoList?.title ?? "Tasks"}</H1>
+				<button
+					className="material-symbols-outlined absolute right-4 top-3"
+					onClick={() => deleteToDoListById({ id: listId })}
+				>
+					delete
+				</button>
 			</Header>
 
 			{isFetching && <SectionPreloader />}
@@ -33,13 +51,22 @@ export const Tasks = (): JSX.Element => {
 						<Link key={id} to={routes.tasks.byId.absolute(id)}>
 							<li
 								className={classNames(
-									"bg-gray-700 hover:bg-gray-800 rounded py-4 pr-3 text-white text-xs pl-4",
+									"bg-gray-700 rounded py-4 pr-3 text-white text-xs pl-4 relative",
 									{
 										"mt-1": idx !== 0,
 									},
 								)}
 							>
 								{title}
+								<button
+									className="material-symbols-outlined absolute right-4 top-3"
+									onClick={e => {
+										e.preventDefault();
+										deleteToDoById({ id });
+									}}
+								>
+									delete
+								</button>
 							</li>
 						</Link>
 					))}
--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,8 +1,13 @@
+import classNames from "classnames";
 import { PropsWithChildren } from "react";
 import { MenuButton } from "./MenuButton";
 
-export const Header = ({ children }: PropsWithChildren): JSX.Element => (
-	<header className="mt-4 mb-5">
+export const Header = ({
+	children,
+	className,
+	...headerProps
+}: PropsWithChildren<JSX.IntrinsicElements["header"]>): JSX.Element => (
+	<header {...headerProps} className={classNames("mt-4 mb-5")}>
 		<MenuButton />
 		{children}
 	</header>
