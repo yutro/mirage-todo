369c59c37b9d39f6ca4a18307216e60b535a18ec
Yury A Troynov <icokie@yandex.ru>
2022-12-29 14:27:33 +0200

    add basic seeds & factory

--- src/mockServer/index.ts
+++ src/mockServer/index.ts
@@ -1,10 +1,28 @@
-import { createServer } from "miragejs";
+import { createServer, Factory } from "miragejs";
 import { createGraphQLHandler } from "@miragejs/graphql";
-import { schema } from "../generated";
+import { FactoryDefinition, ModelDefinition } from "miragejs/-types";
+import { schema, Todo } from "../generated";
 
-const mockServer = createServer({
+type MockServerModels = Readonly<{
+  todo: ModelDefinition<Todo>;
+}>;
+
+type MockServerFactories = Readonly<{
+  todo: FactoryDefinition<Todo>;
+}>;
+
+const mockServer = createServer<MockServerModels, MockServerFactories>({
   routes() {
     this.post("/graphql", createGraphQLHandler(schema, this.schema));
+  },
+  seeds(server) {
+    server.createList("todo", 10);
+  },
+  factories: {
+    todo: Factory.extend({
+      id: (idx) => idx,
+      content: (idx) => `${idx} - some content`,
+    }),
   },
 });
 
